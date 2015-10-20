865d0df68be4832102e6967414541f63df87903b
fabianonunes <fabiano@nunes.me>
2015-10-19 23:04:26 -0200

    primeiro commit

---
+++ generators/app/index.js
@@ -0,0 +1,175 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.generators.Base.extend({
+  
+  prompting: function () {
+
+    var done = this.async();
+
+    this.log(yosay(
+      chalk.red('SenadoCss') + '!'
+    ));
+
+    var prompts = [{
+      type: 'confirm',
+      name: 'includeSenadoCSS',
+      message: 'Deseja incluir o senado.css?',
+      default: true
+    }];
+    
+    this.prompt(prompts, function (props) {
+      this.includeSenadoCSS = props.includeSenadoCSS;
+      done();
+    }.bind(this));
+
+  },
+
+  support : function () {
+
+    var done = this.async();
+
+    var prompts = [{
+      type: 'checkbox',
+      name: 'features',
+      message: 'Que recursos você deseja utilizar?',
+      choices: [{
+        name: 'Bootstrap',
+        value: 'includeBootstrap',
+        checked: true
+      },{
+        name: 'Jade',
+        value: 'includeJade',
+        checked: false
+      }]
+    }];
+
+    this.prompt(prompts, function (answers) {
+      var features = answers.features;
+
+      function hasFeature(feat) {
+        return features && features.indexOf(feat) !== -1;
+      }
+
+      this.includeBootstrap = hasFeature('includeBootstrap');
+      this.includeJade = hasFeature('includeJade');
+
+      done();
+    }.bind(this));
+
+  },
+
+  bootstrapVersion : function () {
+
+    if (this.includeBootstrap) {
+
+      var done = this.async();
+
+      var prompts = [{
+        type: 'list',
+        name: 'bootstrapVersion',
+        message: 'Qual a versão do Bootstrap?',
+        choices: [{
+          name: '3.3.x',
+          value: '^3.3.5'
+        },{
+          name: '2.3.x',
+          value: '^2.3.2'
+        }],
+        default: 0
+      }];
+
+      this.prompt(prompts, function (answers) {
+        this.bootstrapVersion = answers.bootstrapVersion;
+        done();
+      }.bind(this));
+
+    }
+  },
+
+  importFonts: function () {
+    this.importFontsFrom = []
+    if (this.includeSenadoCSS) {
+      this.importFontsFrom.push('senado.css')
+    }
+    if (this.includeBootstrap) {
+      this.importFontsFrom.push('bootstrap')
+    }
+  },
+
+  writing: {
+  
+    gruntfile: function () {
+      this.template('Gruntfile.js');
+    },
+
+    app: function () {
+      this.template('_package.json', 'package.json');
+    },
+
+    git: function () {
+      this.template('gitignore', '.gitignore');
+      this.copy('gitattributes', '.gitattributes');
+    },
+
+    projectfiles: function () {
+      this.fs.copy(
+        this.templatePath('editorconfig'),
+        this.destinationPath('.editorconfig')
+      );
+      this.fs.copy(
+        this.templatePath('jshintrc'),
+        this.destinationPath('.jshintrc')
+      );
+    },
+
+    styles: function () {
+
+      var bootstrapFile;
+      
+      if (this.includeBootstrap) {
+        switch (this.bootstrapVersion) {
+          case  "^3.3.5":
+            bootstrapFile = "bootstrap3.less"
+            break;
+          case  "^2.3.2":
+            bootstrapFile = "bootstrap2.less"
+            break;
+        }
+        this.fs.copy(
+          this.templatePath('app/styles/vendors/' + bootstrapFile),
+          this.destinationPath('app/styles/vendors/bootstrap.less')
+        );
+      }
+
+      if (this.includeSenadoCSS) {
+        this.copy('app/styles/vendors/senado.less');
+      }
+      
+      this.copy('app/styles/utils/variables.less');
+      this.template('app/styles/main.less');
+
+    },
+
+    scripts: function () {
+      this.fs.copy(
+        this.templatePath('app/scripts'),
+        this.destinationPath('app/scripts')
+      );
+    },
+
+    html : function () {
+      if (this.includeJade) {
+        this.copy('app/index.jade')
+      } else {
+        this.copy('app/index.html')
+      }
+    }
+  },
+
+  install: function () {
+    // this.installDependencies();
+  }
+});
---
+++ generators/app/templates/Gruntfile.js
@@ -0,0 +1,210 @@
+/*jshint node:true*/
+
+'use strict';
+
+module.exports = function (grunt) {
+
+    require('time-grunt')(grunt);
+    require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+
+    var config = {
+        app: 'app',
+        dist: 'dist',
+        fonts: <%= JSON.stringify(importFontsFrom) %>
+    };
+
+    grunt.initConfig({
+
+        config: config,
+
+        less: {
+            options: {
+                paths: ['./node_modules'],
+                sourceMap: true,
+                sourceMapRootpath: '../'
+            },
+            base: {
+                files: [{
+                    expand: true,
+                    cwd: '<%%= config.app %>/styles',
+                    src: '*.less',
+                    dest: '<%%= config.dist %>/styles',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        autoprefixer: {
+            options: {
+                browsers: ['> 1%', 'last 2 versions', 'ie 9'],
+                map: true
+            },
+            base: {
+                files: [{
+                    expand: true,
+                    cwd: '<%%= config.dist %>/styles',
+                    src: '{,*/}*.css',
+                    dest: '<%%= config.dist %>/styles'
+                }]
+            }
+        },
+
+        cssmin: {
+            options : {
+                keepSpecialComments: 0
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    src: '<%%= config.dist %>/styles/*.css'
+                }]
+            }
+        },
+
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [ '<%%= config.dist %>' ]
+                }]
+            }
+        },
+
+        <% if (includeJade) { %>
+        jade: {
+            main: {
+                options: {
+                    pretty: true
+                },
+                files: [{
+                    expand: true,
+                    flatten: true,
+                    src: '<%%= config.app %>/*.jade',
+                    dest : '<%%= config.dist %>',
+                    ext: '.html'
+                }]
+            }
+        },
+        <% } else { %>
+        html: {
+
+        },
+        <% }%>
+
+        watch: {
+            
+            gruntfile: {
+                files: ['Gruntfile.js']
+            },
+
+            less: {
+                files: ['<%%= config.app %>/styles/{,*/}*.less'],
+                tasks: ['less', 'autoprefixer']
+            },
+
+            <% if (includeJade) { %>
+            jade: {
+                files: ['<%%= config.app %>/*.jade'],
+                tasks: ['jade']
+            },
+            <% } else { %>
+            html: {
+                files: ['<%%= config.app %>/*.html'],
+                tasks: ['html']
+            },
+            <% } %>
+            
+            livereload: {
+                options: {
+                    livereload: '<%%= connect.options.livereload %>'
+                },
+                files: [
+                    '<%%= config.dist %>/{,*/}*.html',
+                    '<%%= config.dist %>/styles/{,*/}*.css',
+                ]
+            }
+
+        },
+
+        connect: {
+            options: {
+                port: 8000,
+                livereload: 35729,
+                hostname: 'localhost'
+            },
+            dev: {
+                options: {
+                    middleware: function(connect, options, middlewares) {
+                        
+                        middlewares.unshift(function(req, res, next) {
+                            res.setHeader('Access-Control-Allow-Origin', '*');
+                            res.setHeader('Access-Control-Allow-Methods', '*');
+                            next();
+                        });
+                        
+                        var serveStatic = require('serve-static');
+                        middlewares.unshift(serveStatic("dist"))
+
+                        return middlewares;
+
+                    }
+                }
+            },
+            dist: {
+                options: {
+                    base: '<%%= config.dist %>',
+                    livereload: false
+                }
+            }
+        },
+
+        copy: {
+            fontify: {
+                files: [{
+                    expand: true,
+                    dest: '<%%= config.dist %>/fonts',
+                    flatten: true,
+                    src: 'node_modules/{<%%= config.fonts.join(",") %>}/fonts/*'
+                }]
+            }
+        },
+
+        concurrent: {
+            server: [
+                'less', 'copy:fontify' <% if (includeJade) { %>, 'jade'<% } %>
+            ]
+        }
+
+    });
+
+    grunt.registerTask('dev', '--allow-remote para permitir acesso externo', function (target) {
+
+        if (grunt.option('allow-remote')) {
+            grunt.config.set('connect.options.hostname', '0.0.0.0');
+        }
+        if (target === 'dist') {
+            return grunt.task.run(['build', 'connect:dist:keepalive']);
+        }
+
+        grunt.task.run([
+            'clean',
+            'concurrent',
+            'autoprefixer',
+            'connect:dev',
+            'watch'
+        ]);
+
+    });
+
+    grunt.registerTask('build', [
+        'clean:dist',
+        'concurrent:dist',
+        'autoprefixer',
+        'copy:dist',
+    ]);
+
+    grunt.registerTask('default', [
+        'build'
+    ]);
+
+};
